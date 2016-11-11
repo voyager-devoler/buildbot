325fdf8c187404464c32182c231a58e48b599b07
voyager <titov@devoler.com>
2016-11-11 14:47:35 +0200

    progress

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -4,22 +4,251 @@ class Model_Product extends Model_Abstract{
     public $id;
     public $name;
     public $minutes;
-    public $cost_all;
     public $price_one;
     public $base_amount;
+    public $base_level;
+    public $price;
     public $building_id;
+    public $base_quantity = 1;
     public $ingredients = array();
+    public $is_food;
     protected $_tablename = 'mi_products';
- 
+    
+    protected $_allIngredients =  null;
+    
+    public function __construct($object_id) {
+        parent::__construct($object_id);
+        $this->base_quantity = $this->base_amount;
+    }
+
+
     public function fillIngredients()
     {
         $ingredients_data = $this->_db()->select('select ingredient_id, base_quantity from mi_prod_content where prod_id=?d', $this->id);
         foreach ($ingredients_data as $ing)
         {
-            $this->ingredients[$ing['ingredient_id']] = new Model_Ingredient($ing['ingredient_id']);
+            $this->ingredients[$ing['ingredient_id']] = new Model_Product($ing['ingredient_id']);
             $this->ingredients[$ing['ingredient_id']]->base_quantity = $ing['base_quantity'];
             $this->ingredients[$ing['ingredient_id']]->fillIngredients();
         }
+    }
+    
+    public function getLinkHelper()
+    {
+        return "<a href=\"/products.php?p={$this->id}\">{$this->name}</a>";
+    }
+    
+    public function getAllIngredientsAsArray(&$products = array(), $pquantity = 1, $first_call = true)
+    {
+        if (is_null($this->_allIngredients))
+        {
+            if (!isset($products[$this->id]))
+            {
+                $products[$this->id]['p'] = $this;
+                $products[$this->id]['q'] = $this->base_quantity * $pquantity;
+            }
+            else {
+                $products[$this->id]['q']+=$this->base_quantity * $pquantity;
+            }
+            foreach ($this->ingredients as $ingr)
+            {
+                $ingr->getAllIngredientsAsArray($products, $this->base_quantity/$this->base_amount * $pquantity, false);
+            }
+            if ($first_call)
+                $this->_allIngredients = $products;
+        }
+        return $this->_allIngredients;
+    }
+    
+    public function getPrinted()
+    {
+        $html = $this->getLinkHelper();
+        if (isset($this->ingredients))
+        {
+            $html.= "<ul>";
+            foreach ($this->ingredients as $ingredient)
+            {
+                $html.= "<li>[{$ingredient->base_quantity}] ".$ingredient->getPrinted()."</li>";
+            }
+            $html.= "</ul>";
+        }
+        return $html;
+    }
+    
+    public function getAddValue()
+    {
+        $ing_value = 0;
+        foreach ($this->ingredients as $ing)
+        {
+            $ing_value += $ing->price_one * $ing->base_quantity;
+        }
+        return $this->price_one * $this->base_amount - $ing_value;
+    }
+    
+    public function modifyProductToProdLevel(Model_Product $base_product, $target_level)
+    {
+        if ($target_level > $base_product->base_level)
+        {
+            if (false)//($this->is_food)
+            {
+//                for ($i=$base_product->base_level+1;$i<=$target_level;$i++)
+//                {
+//                    $effect1 = $this->tryToReduceIng($base_product,1,0);
+//                    $effect2 = $this->tryToReduceIng($base_product,1,1);
+//                    $effect3 = $this->tryToReduceIng($base_product,1,2);
+//                    $effect4 = $this->tryToIncreaseAmount($base_product);
+//                    $min = false;
+//                    $what = array();
+//                    $profit_lowlimit = 1.05;
+//                    $quantity = 0;
+//                    do
+//                    {
+//                        $quantity++;
+//                        $effect1 = $this->tryToReduceIng($base_product,$quantity,0);
+//                    } while ($effect1 !== false && $effect1['effect']<$profit_lowlimit);
+//                    if ($effect1)    
+//                        if (!$min || $effect1['effect']<$min)
+//                        {
+//                            $min = $effect1['effect'];
+//                            $what = array('todo'=>'reduceIngr', 'ingr_id'=>$effect1['ingr'], 'quantity'=>$quantity);
+//                        }
+//                    $quantity = 0;
+//                    do
+//                    {
+//                        $quantity++;
+//                        $effect2 = $this->tryToReduceIng($base_product,$quantity,1);
+//                    } while ($effect2 !== false && $effect2['effect']<$profit_lowlimit);
+//                    if ($effect2)
+//                        if (!$min || $effect2['effect']<$min)
+//                        {
+//                            $min = $effect2['effect'];
+//                            $what = array('todo'=>'reduceIngr', 'ingr_id'=>$effect2['ingr'], 'quantity'=>$quantity);
+//                        }
+//                    $quantity = 0;
+//                    do
+//                    {
+//                        $quantity++;
+//                        $effect3 = $this->tryToReduceIng($base_product,$quantity,2);
+//                    } while ($effect3 !== false && $effect3['effect']<$profit_lowlimit);
+//                    if ($effect3)
+//                        if (!$min || $effect3['effect']<$min)
+//                        {
+//                            $min = $effect3['effect'];
+//                            $what = array('todo'=>'reduceIngr', 'ingr_id'=>$effect3['ingr'], 'quantity'=>$quantity);
+//                        }
+//                    if ($effect4)
+//                        if (!$min || $effect4['effect']<$min)
+//                        {
+//                            $min = $effect4['effect'];
+//                            $what = array('todo'=>'increaseAmount');
+//                        }
+//                    if (empty($what))
+//                        throw new Exception ("Can't upgrade productivity...");
+//                    if ($what['todo'] == 'reduceIngr')
+//                        $this->ingredients[$what['ingr_id']]->base_quantity -= $what['quantity'];
+//                    else
+//                    {
+//                        $this->base_amount++;
+////                        echo '<pre>';
+////                        var_dump($this->ingredients,'++++++++++++++++++', $base_product->i);
+////                        die();
+//                        foreach ($this->ingredients as $id=>$ing)
+//                            $ing->base_quantity = $base_product->ingredients[$id]->base_quantity;
+//                        //echo '</pre>';
+//                    }
+//                }
+            } else
+            {
+               if ($this->base_amount > 1)
+                    $this->base_amount = $this->base_amount - $this->base_level + $target_level;
+               else
+                    $this->base_amount = floor($this->base_amount + ($target_level - $this->base_level) * 0.34); 
+               foreach ($this->ingredients as $id=>$ing)
+               {
+                   $this->ingredients[$id]->base_quantity = round($this->base_amount/($base_product->base_amount + ($target_level - $this->base_level)*0.1)*$base_product->ingredients[$id]->base_quantity);
+               }
+            }
+        } elseif ($target_level < $base_product->base_level) {
+            $amount_0 = $base_product->base_amount;
+            if ($base_product->base_amount > 1)
+            {
+                $amount_0 = floor($base_product->base_amount/(1+$base_product->base_level/5));
+                $this->base_amount = floor($target_level/$base_product->base_level*($base_product->base_amount - $amount_0)+$amount_0);
+            }
+            $ing_incr_coeff = $base_product->base_level / ($base_product->base_amount - $amount_0 + 1);
+            if ($ing_incr_coeff < 1)
+                $ing_incr_coeff = 1;
+            foreach ($this->ingredients as $id=>$ing)
+            {
+                $ing_current_coeff = $ing_incr_coeff;
+                //echo $this->ingredients[$id]->name.":".$ing_incr_coeff."|";
+                if ($ing_current_coeff - 1  > $base_product->base_level/$base_product->ingredients[$id]->base_quantity)
+                {
+                    $ing_current_coeff = (($base_product->base_level + $base_product->ingredients[$id]->base_quantity) / $base_product->ingredients[$id]->base_quantity * 4 + $ing_incr_coeff)/5;
+                }
+                $this->ingredients[$id]->base_quantity = round(
+                        $base_product->ingredients[$id]->base_quantity * 
+                        (1 + ($ing_current_coeff-1) * ($base_product->base_level - $target_level) / $base_product->base_level));
+            }
+        }
+            
+    }
+    
+    public function tryToReduceIng(Model_Product $base_product, $quantity ,$num)
+    {
+        if (empty($this->ingredients))
+            return false;
+        $ings = array();
+        foreach ($this->ingredients as $ingr)
+        {
+            $ings[] = $ingr;
+        }
+        if (!isset($ings[$num]))
+            return false;
+        $ing = $ings[$num];
+        $old_profit = $this->getAddValue();
+        $ing->base_quantity -= $quantity;
+        $new_profit = $this->getAddValue();
+        if ($ing->base_quantity/$base_product->ingredients[$ing->id]->base_quantity < 0.5)
+        {
+            $ing->base_quantity += $quantity;
+            return false;
+        }
+        $ing->base_quantity += $quantity;
+        return array('effect' => $new_profit/$old_profit, 'ingr'=>$ing->id);
+    }
+    
+    public function tryToIncreaseAmount($base_product)
+    {
+        $old_profit = $this->getAddValue();
+        $this->base_amount ++;
+        $new_profit = $this->getAddValue();
+        $this->base_amount --;
+        return array ('effect' => $new_profit/$old_profit);
+    }
+    
+    public function tryToIncreaseIng(Model_Product $base_product, $quantity ,$num)
+    {
+        if (empty($this->ingredients))
+            return false;
+        $ings = array();
+        foreach ($this->ingredients as $ingr)
+        {
+            $ings[] = $ingr;
+        }
+        if (!isset($ings[$num]))
+            return false;
+        $ing = $ings[$num];
+        $old_profit = $this->getAddValue();
+        $ing->base_quantity += $quantity;
+        $new_profit = $this->getAddValue();
+        if ($ing->base_quantity/$base_product->ingredients[$ing->id]->base_quantity > 2)
+        {
+            $ing->base_quantity -= $quantity;
+            return false;
+        }
+        $ing->base_quantity -= $quantity;
+        return array('effect' => abs(($old_profit-$new_profit)/$base_product->getAddValue()), 'ingr'=>$ing->id);
     }
 }
 
--- www/products.php
+++ www/products.php
@@ -1,8 +1,157 @@
 <?php
 require_once '../config.php';
+?>
+<html>
+  <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-if (!isset($_GET['p']))
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link rel="stylesheet" href="starter-template.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </head>
+  <body>
+      <!-- <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Home</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/products.php?products">products</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+    <!--  </div>
+    </nav> -->
+
+    <div class="container">
+
+      <div class="starter-template">
+          
+          <div class="row">
+<?php
+
+$prod_ids = dbLink::getDB()->selectCol('select id from products where 1');
+$prods = array();
+foreach ($prod_ids as $id)
 {
-    dbLink::getDB()->select('select * from products where 1');
-    
+    $prods[$id] = new Model_Product($id);
+    $prods[$id]->fillIngredients();
 }
+$prods_list_html = "";
+$prod_table_html = "<div class='row'><div class='col-md-4'>продукт</div><div class='col-md-2'>реф.уровень.</div><div class='col-md-2'>минуты</div><div class='col-md-2'>цена единицы</div><div class='col-md-2'>партия</div>";
+foreach ($prods as $prod)
+{
+    $prods_list_html .="<div>".$prod->getLinkHelper()."</div>";
+    $prod_table_html .="<div class='col-md-4'>{$prod->getLinkHelper()}</div>";
+    $prod_table_html .="<div class='col-md-2'>{$prod->base_level}</div>";
+    $prod_table_html .="<div class='col-md-2'>{$prod->minutes}</div>";
+    $prod_table_html .="<div class='col-md-2'>{$prod->price_one}</div>";
+    $prod_table_html .="<div class='col-md-2'>{$prod->base_amount}</div>";
+}
+$prod_table_html.="</div>";
+$prod_structure_html = "";
+$prod_all_ingridients_html = "";
+if (isset($_GET['p']))
+{
+    $product = $prods[$_GET['p']];
+    $prod_structure_html = $product->getPrinted();
+    $allIngredients = $product->getAllIngredientsAsArray();
+    foreach ($allIngredients as $ingr)
+    {
+        $prod_all_ingridients_html.= "<div>{$ingr['p']->getLinkHelper()} - {$ingr['q']}</div>";
+    }
+    $progress = array();
+//    $progress[$product->base_level] = array('amount'=>$product->base_amount, 'addval'=>$product->getAddValue());
+//    foreach ($product->ingredients as $ingr) /* @var $ingr Model_Product */
+//    {
+//        $progress[$product->base_level]['ings'][$ingr->id] = $ingr->base_quantity;
+//    }
+    for ($i=0;$i<=20;$i++)
+    {
+        if ($i == $product->base_level)
+        {
+            $current_prod = $product;
+        } else {
+            $current_prod = new Model_Product($product->id);
+            $current_prod->fillIngredients();
+            $current_prod->modifyProductToProdLevel($product, $i);
+        }
+        $progress[$i] = array('amount'=>$current_prod->base_amount, 'addval'=>$current_prod->getAddValue());
+        foreach ($current_prod->ingredients as $ingr) /* @var $ingr Model_Product */
+        {
+            $progress[$i]['ings'][$ingr->id] = $ingr->base_quantity;
+        }
+    }
+    $prod_progress_html = "";
+    $prod_progress_html[-1] = "<div class='col-md-2'>level</div><div class='col-md-2'>amount</div>";
+    foreach ($product->ingredients as $ing)
+    {
+        $prod_progress_html[-1] .= "<div class='col-md-2'>{$ing->name}</div>"; 
+    }
+    $prod_progress_html[-1] .= "<div class='col-md-2'>add.val.</div>";
+    foreach ($progress as $level=>$level_data)
+    {
+        $prod_progress_html[$level] = "<div class='col-md-2'>{$level}</div><div class='col-md-2 bg-warning'>{$level_data['amount']}</div>";
+        foreach ($level_data['ings'] as $ing_count)
+        {
+            $prod_progress_html[$level].= "<div class='col-md-2'>{$ing_count}</div>";
+        }
+        $prod_progress_html[$level] .= "<div class='col-md-2'>{$level_data['addval']}</div>";
+    }
+}
+if (!isset($product))
+{
+    echo "<div class='col-md-12'>продукты ";
+?>
+<form class="form-inline" role="form">
+  <div class="form-group">
+    <label class="sr-only" for="datetimefield">date time</label>
+    <input type="datetime" class="form-control" id="datetimefield" placeholder="YYYY-MM-DD H:i:s" />
+  </div>
+  <button type="submit" class="btn btn-default">что изменилось?</button>
+</form>
+<?php
+    echo $prod_table_html;
+    echo "<div class='col-md-10'>";
+    echo "<div class='row'>";
+}
+if (isset($product))
+{
+    echo "<div class='col-md-2'><p><a href='/products.php'>список продуктов</a></p>";
+    echo $prods_list_html;
+    echo "</div>";
+    echo "<div class='col-md-10'>";
+    echo "<div class='row'>";
+    echo "<div class='col-md-6'><h2>{$product->name}</h2>";
+    echo $prod_structure_html;
+    echo "</div>";
+    echo "<div class='col-md-6'>Расчет количества ресурсов на партию для референсных уровней:";
+    echo $prod_all_ingridients_html;
+    echo "</div></div>";
+    echo "<div class'row'>";
+    echo "<div class='col-md-12'><h3>производительность</h3>";
+    foreach ($prod_progress_html as $level=>$progress_line)
+    {
+        $color = "";
+        if ($level == $product->base_level)
+            $color = ' bg-success';
+        echo "<div class='row{$color}'>{$progress_line}</div>";
+    }
+    echo "</div></div>";
+}
+?>
+     </div>
+
+    </div>
+  </body>
+</html>
